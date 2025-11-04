a470004fb1ce2a12393123a3026f659efc9904b0
smileblu <dlalth5610@gmail.com>
2025-11-04 22:34:42 +0900

    Revert "Judy5_week5"

--- umcMission/src/constants/key.ts
+++
@@ -1,4 +0,0 @@
-// 상수화 시킬 값들
-export const LOCAL_STORAGE_KEY = {
-    accessToken: "accessToken",
-};
--- umcMission/src/pages/HomePage.tsx
+++
@@ -1,5 +0,0 @@
-const HomePage = () => {
-    return <div className="h-dvh flex flex-col">홈페이지</div>;
-};
-
-export default HomePage;
